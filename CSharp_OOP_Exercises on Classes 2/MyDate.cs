189ebec752dba5d59414db094cd4011395a91a3c
Jan Matušek <jan.matusek94@gmail.com>
2023-04-22 13:35:28 +0200

    Add project files.

---
+++ CSharp_OOP_Exercises on Classes 2/MyDate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_OOP_Exercises_on_Classes_2 {
+	internal class MyDate {
+
+	}
+}
---
+++ CSharp_OOP_Exercises on Classes 2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csharp_OOP_Exercises2;
+
+class Hlavni {
+	public static void Main() {
+		//Circle.Mainx();
+		//Account.Mainx();
+		//Date.Mainx();
+		//Time.Mainx();
+	}
+}
